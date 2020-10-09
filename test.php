3e5291f9f71342c7ff74a968b7734aadfcc3ba0b
Chan Myae Aung <chanmyaetum3@gmail.com>
2020-10-09 22:39:04 +0630

    Test

---
+++ test.php
@@ -0,0 +1,44 @@
+<?php
+
+
+echo "Hello world";
+print "Hello Myanmar";
+printf("Hello Thailand",true);
+echo "<hr/>";
+
+//Variable
+$thint ="thint thint tun";
+echo $thint;
+echo "<hr/>";
+
+//Concandination
+$val1 = "Golden Land";
+$val2 = "Myanmar";
+echo $val1." ".$val2;
+echo "<hr/>";
+
+//Arithmetic Operator
+$num1=10;
+$num2=2;
+echo $num1+$num2;
+echo "<br/>";
+echo $num1-$num2;
+echo "<br/>";
+echo $num1*$num2;
+echo "<br/>";
+echo $num1/$num2;
+echo "<hr/>";
+
+//Ternery Operator
+echo (10 > 1)? "aung aung":"zaw zaw";
+echo"<hr/>";
+
+//Comparison Operators ==,===datatype check,!=,<,>,<=,>=
+$a=10;
+$b=20;
+if($a < $b){
+    echo "Condition pass";
+    
+}
+
+?>
